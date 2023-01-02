d7d4b7c53ac81d087ff2906075b38074377816c5
Bryan Bell <bryanbellabc@gmail.com>
2023-01-02 00:35:36 -0600

    feat: :sparkles: User can pause the timer.

--- src/lib/models/classes/custom-stores/timer.test.ts
+++ src/lib/models/classes/custom-stores/timer.test.ts
@@ -16,6 +16,11 @@ describe('Timer', () => {
 		expect(timer).toBeInstanceOf(Timer);
 	});
 
+	it('should have a property named "isPaused" with a boolean value ', () => {
+		const isPaused = get(timer.isPaused$);
+		expect(typeof isPaused).toBe('boolean');
+	});
+
 	it('should be paused by default', () => {
 		expect(get(timer.isPaused$)).toBe(true);
 	});
@@ -46,5 +51,65 @@ describe('Timer', () => {
 		timeLeftInMinutes = get(timer.timeLeftInMinutes$);
 
 		expect(timeLeftInMinutes).toBe('24:00');
+	});
+
+	it('should pause countdown after pausing the timer', () => {
+		vi.useFakeTimers();
+		let timeLeftInMinutes = get(timer.timeLeftInMinutes$);
+		expect(timeLeftInMinutes).toBe('25:00');
+
+		timer.start();
+
+		// Advance the timer by 30 seconds
+		vi.advanceTimersByTime(30000);
+		timeLeftInMinutes = get(timer.timeLeftInMinutes$);
+
+		expect(timeLeftInMinutes).toBe('24:30');
+
+		timer.pause();
+
+		// Advance the timer by 30 seconds
+		vi.advanceTimersByTime(30000);
+		timeLeftInMinutes = get(timer.timeLeftInMinutes$);
+
+		// The timer should not have advanced
+		expect(timeLeftInMinutes).toBe('24:30');
+	});
+
+	it('should have "isPaused: false" after starting the timer', () => {
+		let isPaused = get(timer.isPaused$);
+		expect(isPaused).toBe(true);
+
+		timer.start();
+		isPaused = get(timer.isPaused$);
+		expect(isPaused).toBe(false);
+	});
+
+	it('should have "isPaused: true" after pausing the timer', () => {
+		let isPaused = get(timer.isPaused$);
+		expect(isPaused).toBe(true);
+
+		timer.start();
+		isPaused = get(timer.isPaused$);
+		expect(isPaused).toBe(false);
+
+		timer.pause();
+		isPaused = get(timer.isPaused$);
+		expect(isPaused).toBe(true);
+	});
+
+	it('should have "isPaused: true" after the timer ends', () => {
+		let isPaused = get(timer.isPaused$);
+		const minutes = 25;
+		const milliseconds = minutes * 60 * 1000;
+
+		vi.useFakeTimers();
+		timer.start();
+		isPaused = get(timer.isPaused$);
+		expect(isPaused).toBe(false);
+
+		vi.advanceTimersByTime(milliseconds);
+		isPaused = get(timer.isPaused$);
+		expect(isPaused).toBe(true);
 	});
 });
