9ec2a7a4b254ecd84bf85b217c8c277f4c8cfe0e
Bryan Bell <bryanbellabc@gmail.com>
2023-01-01 22:54:59 -0600

    chore: :rotating_light: Install & config eslint with custom rules.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,71 @@
 module.exports = {
 	root: true,
-	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020
-	},
 	env: {
 		browser: true,
 		es2017: true,
-		node: true
-	}
+		node: true,
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2020,
+		tsconfigRootDir: __dirname,
+		project: ['./tsconfig.json'],
+		extraFileExtensions: ['.svelte'],
+	},
+	plugins: ['svelte', '@typescript-eslint', 'import', 'simple-import-sort', 'unused-imports'],
+	ignorePatterns: ['*.cjs'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier',
+		'plugin:svelte/recommended',
+		'plugin:import/errors',
+		'plugin:import/warnings',
+		'plugin:import/typescript',
+		"eslint-config-codely/typescript",
+	],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+			},
+			rules: {
+				'no-inner-declarations': 'off'
+			}
+		},
+	],
+	settings: {},
+	rules: {
+		'no-console': 'off',
+		semi: ['warn', 'always'],
+		'import/first': 'error',
+		'import/newline-after-import': 'error',
+		'import/no-duplicates': 'error',
+		'import/no-unresolved': 'off',
+		'prefer-const': 'error',
+		'simple-import-sort/exports': 'error',
+		'simple-import-sort/imports': 'error',
+		'unused-imports/no-unused-imports': 'error',
+		'unused-imports/no-unused-vars': [
+			'warn',
+			{
+				vars: 'all',
+				varsIgnorePattern: '^_',
+				args: 'after-used',
+				argsIgnorePattern: '^_',
+			},
+		],
+		// 'key-spacing': [
+		// 	'error', 
+		// 	{ 
+		// 		beforeColon: true, 
+		// 		align: 'colon',
+		// 		mode: "strict"
+		// 	}
+		// ],
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+	},
 };
